033edf71da922c1bfb2c3ef4f620a89355fb5b2e
blyang <clear_d@163.com>
2016-04-14 23:34:59 +0800

    2'st update

--- BinNode/Order.java
+++ BinNode/Order.java
@@ -1,4 +1,7 @@
 package com.BinNode;
+
+import java.util.Stack;
+
 /**
  * 前序遍历二叉树
  *
@@ -35,13 +38,112 @@ public class Order {
 		System.out.print( root.Element + "  " );
 	}
 	
+	//前序遍历，不用递归实现，使用栈来实现
+	private static void preOrder1(BinNode root){
+		Stack stack = new Stack();
+		BinNode temp = root;
+		
+		//入栈
+		while( temp != null ){
+			System.out.print( temp.Element + "  " );
+			if( temp.Right != null ){
+				stack.push(temp.Right);
+			}
+			temp = temp.Left;
+		}
+		
+		//出栈
+		while( stack.size() > 0 ){
+			temp = (BinNode) stack.pop();
+			while( temp != null ){
+				System.out.print(temp.Element + "  " );
+				if( temp.Right != null ){
+					stack.push(temp.Right);
+				}
+				temp = temp.Left;
+			}
+		}
+	}
+	
+	
+	//中序遍历,不用递归实现，使用栈区来实现
+	private static void middleOrder1( BinNode root ){
+		Stack stack = new Stack();
+		BinNode temp = root;
+		
+		//入栈
+		while( temp != null ){
+			if( temp != null ){
+				stack.push(temp);
+			}
+			temp = temp.Left;
+		}
+		
+		//出栈
+		while( stack.size() > 0 ){
+			temp = (BinNode) stack.pop();
+			System.out.print( temp.Element + "  ");
+			if( temp.Right != null ){
+				temp = temp.Right;
+				stack.push(temp);
+				while( temp != null ){
+					if( temp.Left != null){
+						stack.push(temp.Left);
+					}
+					temp = temp.Left;
+				}
+			}
+		}
+	}
+	
+	
+	//后序遍历，不用递归实现，使用栈来记录。这里有点繁，根节点还是要重新装载进栈
+	private static void postOrder1( BinNode root ){
+		Stack stack = new Stack();
+		BinNode temp = root;
+		
+		//入栈
+		while(temp != null){
+			if(temp != null){
+				stack.push(temp);
+			}
+			temp = temp.Left;
+		}
+		
+		//出栈
+		while( stack.size() > 0 ){
+			BinNode lastVisit = temp;
+			temp = (BinNode) stack.pop();
+			if(temp.Right == null || temp.Right == lastVisit){
+				System.out.print( temp.Element + "  " );
+			} else if ( temp.Left == lastVisit ){
+				stack.push(temp);  //再装载进去
+				temp = temp.Right;
+				stack.push(temp);
+				while( temp != null ){
+					if( temp.Left != null ){
+						stack.push(temp.Left);
+					}
+					temp = temp.Left;
+				}
+			}
+		}
+	}
+	
+	
 	//测试
 	public static void main(String[] args) {
 		preOrder(Common.root);
-		System.out.println("\n------------------");
+		System.out.println("\n-------------------");
 		middleOrder(Common.root);
-		System.out.println("\n------------------");
+		System.out.println("\n-------------------");
 		postOrder(Common.root);
+		System.out.println("\n-------------------");
+		preOrder1(Common.root);
+		System.out.println("\n-------------------");
+		middleOrder1(Common.root);
+		System.out.println("\n-------------------");
+		postOrder1(Common.root);
 	}
 	
 }
